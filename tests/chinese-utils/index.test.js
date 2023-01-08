f29b3edec1b1f3487d9c24586a41fae5a2fc27c3
shuntian <978987373@qq.com>
2023-01-08 22:22:59 +0800

    chore: update test ci

--- tests/chinese-utils/index.test.js
+++ tests/chinese-utils/index.test.js
@@ -1,3 +1,19 @@
-import { chinese2FirstLatter } from '../../src/chinese-utils';
+import { chinese2FirstLetter } from '../../src/chinese-utils';
 
-test('test chinese 2 first letters', () => {});
+test('test chinese 2 first letters', () => {
+  const chinese = '柿子红了';
+  const letters = chinese2FirstLetter(chinese)[0];
+  expect(letters).toBe('SZHL');
+});
+
+test('test chinese 2 first letters', () => {
+  const chinese = '我命由我不由天';
+  const letters = chinese2FirstLetter(chinese)[0];
+  expect(letters).toBe('WMYWBYT');
+});
+
+test('test chinese 2 first letters', () => {
+  const chinese = '天山鸟飞绝';
+  const letters = chinese2FirstLetter(chinese)[0];
+  expect(letters).toBe('TSNFJ');
+});
